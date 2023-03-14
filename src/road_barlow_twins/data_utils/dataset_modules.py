b22657ddab911c7b3bcc97005913336d24e6c23d
roydenwa <royden-wagner@outlook.com>
2023-03-14 20:48:13 +0100

    Slice train dataset

--- src/road_barlow_twins/data_utils/dataset_modules.py
+++ src/road_barlow_twins/data_utils/dataset_modules.py
@@ -1,7 +1,8 @@
 import os
+import torch
 import pytorch_lightning as pl
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, data_utils
 
 from l5kit.configs import load_config_data
 from l5kit.data import LocalDataManager, ChunkedDataset
@@ -10,11 +11,20 @@ from l5kit.rasterization import build_rasterizer
 
 
 class WovenPredictionDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=32, num_dataloader_workers=8, pin_memory=True):
+    def __init__(
+        self,
+        batch_size=32,
+        num_dataloader_workers=8,
+        pin_memory=True,
+        num_train_samples=500_000,
+        step_size_slicing=10,
+    ):
         super().__init__()
         self.batch_size = batch_size
         self.num_dataloader_workers = num_dataloader_workers
         self.pin_memory = pin_memory
+        self.num_train_samples = num_train_samples
+        self.step_size_slicing = step_size_slicing
 
     def prepare_data(self):
         pass
@@ -36,12 +46,22 @@ class WovenPredictionDataModule(pl.LightningDataModule):
             ).open()
             train_dataset = AgentDataset(cfg, train_zarr, rasterizer)
 
-            self.train_set = train_dataset
-
-            val_zarr = ChunkedDataset(dm.require(cfg["val_data_loader"]["key"])).open()
-            val_dataset = AgentDataset(cfg, val_zarr, rasterizer)
-
-            self.val_set = val_dataset
+            self.train_set = data_utils.Subset(
+                train_dataset,
+                torch.arange(
+                    start=0,
+                    end=self.num_train_samples * self.step_size_slicing,
+                    step=self.step_size_slicing,
+                ),
+            )
+            self.val_set = data_utils.Subset(
+                train_dataset,
+                torch.arange(
+                    start=self.num_train_samples * self.step_size_slicing,
+                    end=self.num_train_samples * self.step_size_slicing + 10_000,
+                    step=1,
+                ),
+            )
 
     def train_dataloader(self):
         return DataLoader(
