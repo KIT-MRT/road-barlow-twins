7c830cb5ce539bc39ad127cc2fd9e3ba12476c98
roydenwa <royden-wagner@outlook.com>
2023-05-19 12:09:09 +0200

    Remove modules for woven dataset

--- src/road_barlow_twins/data_utils/dataset_modules.py
+++ src/road_barlow_twins/data_utils/dataset_modules.py
@@ -5,12 +5,6 @@ import pytorch_lightning as pl
 
 from torch.utils.data import DataLoader, Subset, Dataset
 
-from l5kit.configs import load_config_data
-from l5kit.data import LocalDataManager, ChunkedDataset
-from l5kit.dataset import AgentDataset, EgoAgentDatasetVectorized
-from l5kit.rasterization import build_rasterizer
-from l5kit.vectorization.vectorizer_builder import build_vectorizer
-
 from .raster_barlow_twins_transform import BarlowTwinsTransform
 
 
@@ -76,80 +70,6 @@ class WaymoPredictionBarlowRasterDataModule(pl.LightningDataModule):
                 self.train_path, limit=self.train_limit
             )
             self.val_set = WaymoBarlowRasterLoader(self.val_path, limit=self.val_limit)
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_dataloader_workers,
-            pin_memory=self.pin_memory,
-            persistent_workers=True,
-            drop_last=True,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_dataloader_workers,
-            pin_memory=self.pin_memory,
-            persistent_workers=True,
-            drop_last=True,
-        )
-
-
-class WovenPredictionDataModule(pl.LightningDataModule):
-    def __init__(
-        self,
-        batch_size=32,
-        num_dataloader_workers=8,
-        pin_memory=True,
-        num_train_samples=500_000,
-        step_size_slicing=10,
-    ):
-        super().__init__()
-        self.batch_size = batch_size
-        self.num_dataloader_workers = num_dataloader_workers
-        self.pin_memory = pin_memory
-        self.num_train_samples = num_train_samples
-        self.step_size_slicing = step_size_slicing
-
-    def prepare_data(self):
-        pass
-
-    def setup(self, stage: str):
-        os.environ[
-            "L5KIT_DATA_FOLDER"
-        ] = "/p/project/hai_mrt_pc/woven-prediction-dataset/"
-
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit":
-            dm = LocalDataManager(None)
-            cfg = load_config_data(
-                "/p/home/jusers/wagner20/juwels/road-barlow-twins/src/road_barlow_twins/data_utils/agent_motion_config.yaml"
-            )
-            rasterizer = build_rasterizer(cfg, dm)
-            train_zarr = ChunkedDataset(
-                dm.require(cfg["train_data_loader"]["key"])
-            ).open()
-            train_dataset = AgentDataset(cfg, train_zarr, rasterizer)
-
-            self.train_set = Subset(
-                train_dataset,
-                torch.arange(
-                    start=0,
-                    end=self.num_train_samples * self.step_size_slicing,
-                    step=self.step_size_slicing,
-                ),
-            )
-            self.val_set = Subset(
-                train_dataset,
-                torch.arange(
-                    start=self.num_train_samples * self.step_size_slicing,
-                    end=self.num_train_samples * self.step_size_slicing + 10_000,
-                    step=1,
-                ),
-            )
 
     def train_dataloader(self):
         return DataLoader(
@@ -268,76 +188,4 @@ class WaymoPredictionDataModule(pl.LightningDataModule):
             pin_memory=self.pin_memory,
             persistent_workers=True,
             drop_last=True,
-        )
-
-
-class WovenPredictionGraphDataModule(pl.LightningDataModule):
-    def __init__(
-        self,
-        batch_size=32,
-        num_dataloader_workers=8,
-        pin_memory=True,
-        num_train_samples=500_000,
-        step_size_slicing=10,
-    ) -> None:
-        super().__init__()
-        self.batch_size = batch_size
-        self.num_dataloader_workers = num_dataloader_workers
-        self.pin_memory = pin_memory
-        self.num_train_samples = num_train_samples
-        self.step_size_slicing = step_size_slicing
-
-    def prepare_data(self):
-        pass
-
-    def setup(self, stage: str):
-        os.environ[
-            "L5KIT_DATA_FOLDER"
-        ] = "/p/project/hai_mrt_pc/woven-prediction-dataset/"
-
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit":
-            dm = LocalDataManager(None)
-            cfg = load_config_data(
-                "/p/home/jusers/wagner20/juwels/road-barlow-twins/src/road_barlow_twins/data_utils/save_path_net_config.yaml"
-            )
-            vectorizer = build_vectorizer(cfg, dm)
-            train_zarr = ChunkedDataset(
-                dm.require(cfg["train_data_loader"]["key"])
-            ).open()
-            train_dataset = EgoAgentDatasetVectorized(cfg, train_zarr, vectorizer)
-
-            self.train_set = Subset(
-                train_dataset,
-                torch.arange(
-                    start=0,
-                    end=self.num_train_samples * self.step_size_slicing,
-                    step=self.step_size_slicing,
-                ),
-            )
-            self.val_set = Subset(
-                train_dataset,
-                torch.arange(
-                    start=self.num_train_samples * self.step_size_slicing,
-                    end=self.num_train_samples * self.step_size_slicing + 10_000,
-                    step=1,
-                ),
-            )
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_dataloader_workers,
-            pin_memory=self.pin_memory,
-            persistent_workers=True,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_dataloader_workers,
-            pin_memory=self.pin_memory,
-            persistent_workers=True,
         )
