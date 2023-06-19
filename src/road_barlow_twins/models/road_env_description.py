cb8bef487c17e459ec2ae66031a302b8a5c04008
roydenwa <royden-wagner@outlook.com>
2023-06-19 18:58:32 +0200

    Add inference func

--- src/road_barlow_twins/models/road_env_description.py
+++ src/road_barlow_twins/models/road_env_description.py
@@ -662,3 +662,35 @@ def feed_forward(dim_input: int = 512, dim_feedforward: int = 2048) -> nn.Module
         nn.ReLU(),
         nn.Linear(dim_feedforward, dim_input),
     )
+
+
+def red_motion_inference(model, batch, device="cuda", return_all_numpy=False):
+    is_available = batch["future_ego_trajectory"]["is_available"].to(device)
+    y = batch["future_ego_trajectory"]["trajectory"].to(device)
+
+    env_idxs_src_tokens = batch["sample_a"]["idx_src_tokens"].to(device)
+    env_pos_src_tokens = batch["sample_a"]["pos_src_tokens"].to(device)
+    env_src_mask = batch["src_attn_mask"].to(device)
+    ego_idxs_semantic_embedding = batch["past_ego_trajectory"][
+        "idx_semantic_embedding"
+    ].to(device)
+    ego_pos_src_tokens = batch["past_ego_trajectory"]["pos_src_tokens"].to(device)
+
+    confidences_logits, logits = model(
+        env_idxs_src_tokens,
+        env_pos_src_tokens,
+        env_src_mask,
+        ego_idxs_semantic_embedding,
+        ego_pos_src_tokens,
+    )
+    confidences = torch.softmax(confidences_logits, dim=1)
+
+    if not return_all_numpy:
+        return confidences, logits
+    else:
+        logits_np = logits.squeeze(0).cpu().numpy()
+        confidences_np = confidences.squeeze(0).cpu().numpy()
+        is_available_np = is_available.squeeze(0).long().cpu().numpy()
+        y_np = y.squeeze(0).cpu().numpy()
+
+        return logits_np, confidences_np, is_available_np, y_np
